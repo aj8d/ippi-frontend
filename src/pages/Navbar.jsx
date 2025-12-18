259c432fe3cfa3dfd51a142c9f0df2ed7a436680
246003 <246003@sapporo-mirai.ac.jp>
2025-12-18 12:38:06 +0900

    WIP: floatingButtonの仮実装

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,15 +1,23 @@
 import { Link, useLocation } from 'react-router-dom';
-import { ArrowRight } from 'lucide-react';
+import { ArrowRight, Menu } from 'lucide-react';
 import { useState } from 'react';
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const navbarMain = document.querySelector('.navbar-main');
+  const navbarHamburger = document.querySelector('.navbar-hamburger');
+
   const location = useLocation();
 
   return (
     <>
-      <nav className="mx-auto max-w-sm flex place-items-stretch justify-center gap-8 rounded-xl bg-white p-6 shadow-lg m-2">
+      <div className="flex items-center">
+        <div className="fixed z-50 navbar-hamburger mx-auto max-w-20 min-y-20 flex items-center rounded-full bg-sky-400 shadow-lg m-2 p-3">
+          <Menu color="white" size={30} />
+        </div>
+      </div>
+      <nav className="navbar-main mx-auto max-w-sm flex place-items-stretch justify-center gap-8 rounded-xl bg-white p-6 shadow-lg m-2 ">
         <Link
           to="/home"
           className={location.pathname === '/home' ? 'text-sky-500 py-2' : 'text-gray-500 py-2'}
