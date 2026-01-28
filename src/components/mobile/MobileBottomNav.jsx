715eb605642e37b8feb297b4f91af7548ebe68ee
azuuuuu <azuuuuu.0301@gmail.com>
2026-01-28 21:11:53 +0900

    chnage: モバイル、タブレットサイズの際にタイマー機能のみ利用できるように変更

--- src/components/mobile/MobileBottomNav.jsx
+++ src/components/mobile/MobileBottomNav.jsx
@@ -75,7 +75,7 @@ export default function MobileBottomNav() {
   return (
     <>
       {/* ボトムナビゲーション */}
-      <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 z-50 md:hidden">
+      <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 z-50 lg:hidden">
         <div className="flex items-center justify-around h-16">
           {/* メインナビゲーション */}
           {navItems.map((item) => {
